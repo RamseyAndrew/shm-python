f96c62db5746e70301891c5ac8c6dc502ab0d182
Arthur-codeX <daniel.karanja239@gmail.com>
2025-08-04 16:58:09 +0300

    completed loops

--- intro/loop.py
+++ intro/loop.py
@@ -32,3 +32,12 @@ for i in range(10,2,-1):
 
 for i in range(0,1000,2):
    print("All even numbers",i)
+
+fruits=["Mango","Papaya","Orange"]
+
+for fruit in fruits:
+   print(fruit)
+
+for i in range(0,3):
+    fruit=fruits[i]
+    print(fruit)
