8a3ac59997e3fd927f91a8435660452c03e6a42d
Arthur-codeX <daniel.karanja239@gmail.com>
2025-08-04 15:43:21 +0300

    created datatypes

--- intro/data_type.py
+++ intro/data_type.py
@@ -1,2 +1,19 @@
 # snake case
 # snake_case
+#indentation
+
+x=10 #Integer 
+pi=3.142  #Floating
+myName="Daniel" #String
+myName1='Daniel' #String
+is_happy=True # python True/False js true/false
+no_value=None # None is the equivalent to Null in js
+
+#Advanced Datatypes Bracket notation
+my_list=[10,10,"string",True,None,123,[]] # List<Array> 
+my_set={1,2,3,3,"Hello","Hello"} #Set(unique stuff only) and Array
+my_tuple=(1,3,4,5,"Apple") #tuple my_tuple[4]=Apple
+my_obj={"key":"value","age":23} #Dictionary Key<->value my_obj["key"]
+
+## Print ALL Value: python print() >---< js<console.log> 
+
