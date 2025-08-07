64fdcfe452212726e3f4537036f89c34d8b6cf8a
Arthur-codeX <daniel.karanja239@gmail.com>
2025-08-07 10:48:59 +0300

    started using self

--- classes/initializer.py
+++ classes/initializer.py
@@ -7,15 +7,32 @@
 #method function is inside a class
 
 #method your create you have to hhave thhe self: keyworkd
+
 class Human():
 
-    def __init__(self):
+    def __init__(self,gender,name):
         print("The initializer wass called")
+        
+        self.gender=gender
+        self.name=name
+        if self.gender=="Male":
+            self.ribs=24
+            self.curse="Suffer"
+        else :
+          self.ribs=23
+          self.curse="Pain"
 
     def another_one(self):
         print("This is another method")
     
-adam=Human() #object from a class
-# adam.__init__()
-#adam.__init__()
-# adam.another_one()
+adam=Human(name="adam",gender="Male") #object from a class
+print("name",adam.name)
+print("gender",adam.gender)
+print("ribs",adam.ribs)
+print("ribs",adam.curse)
+print("")
+eve=Human(name="eve",gender="Female")
+print("name",eve.name)
+print("gender",eve.gender)
+print("ribs",eve.ribs)
+print("curse",eve.curse)
